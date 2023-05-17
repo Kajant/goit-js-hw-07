881d07633ac22340ba1a58df0c0ecb63d9d6213a
Kajetan Antoszkiewicz <wet.kajetan.antoszkiewicz@gmail.com>
2023-05-17 14:40:10 +0200

    task 2 -simple-lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
+'use strict';
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery")
+const picture = galleryItems.map((image) => 
+`<li class="gallery__item">
+    <a class="gallery__link" href="${image.original}">
+    <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+    </a>
+</li>`)
+  .join("");
+gallery.insertAdjacentHTML("beforeend", picture)
+
+gallery.addEventListener("click", getOriginalImg);
+function getOriginalImg(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+    const lightbox = new SimpleLightbox('.gallery a', {
+        captionsData: "alt",
+        captionPosition: "bottom",
+        captionDelay: 250,
+    });
+};
